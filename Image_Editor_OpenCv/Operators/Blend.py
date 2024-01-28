592b05c262ef0ec892cc5a0ffb5f18e73e6acaf1
AJ-SM <154649323+AJ-SM@users.noreply.github.com>
2024-01-28 22:56:07 +0530

    Functionallity Added

--- Image_Editor_OpenCv/Operators/Blend.py
+++ Image_Editor_OpenCv/Operators/Blend.py
@@ -3,14 +3,15 @@ import cv2 as cv
 # Defining Blending Function -- > 
 
 def Img_Blend(img1,img2):
-    
-    try:
-   
-            alpha = float(input("Enter Weight_1 : "))
-            beta = float(input("Enter Weight_2 : "))
-            blend = alpha*img1 + beta*img2
-            return blend
-    except :
-       print("Image Dimension Aren't Matching,Please Add Images Of Same Dimension !! ")
+        if img1.size == img2.size:
+                 alpha = float(input("Enter Weight_1 : "))
+                 beta = float(input("Enter Weight_2 : "))
+                 blend = alpha*img1 + beta*img2
+                 return blend
+             
+                        
+        else :
+                print("Image Dimension Aren't Matching,Please Add Images Of Same Dimension !! ")
+                return None
 
     
