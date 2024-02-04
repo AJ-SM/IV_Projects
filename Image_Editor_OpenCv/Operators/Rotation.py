4c55becf442202a7dd2c29660d8317735ac61094
kartik-mundkar <kartikdm2005@gmail.com>
2024-02-04 11:49:24 +0530

    Update Rotation.py

--- Image_Editor_OpenCv/Operators/Rotation.py
+++ Image_Editor_OpenCv/Operators/Rotation.py
@@ -28,7 +28,7 @@ def rotate_at_angle(image, direction, angle):
             actual_x=round(new_x+rotated_img1_center[0])
             rotated_img1[actual_y,actual_x]=img1[i,j]
     
-    return img1,rotated_img1
+    return img1,rotated_img1,angle
 
 
 
@@ -53,17 +53,20 @@ def image_input():
     return (rotate_at_angle(image,image_rotation_direction,float(image_rotation_angle)))
 
 
-def image_show(image,rotated_image):
+def image_show(image,rotated_image,image_rotation_angle):
+    plt.suptitle("Image Rotation")
     plt.subplot(1,2,1)
     plt.imshow(image)
+    plt.title("Original Image")
     plt.axis('off')
     plt.subplot(1,2,2)
     plt.imshow(rotated_image)
+    plt.title(f"{str(image_rotation_angle) + "°"} Rotated Image")
     plt.axis('off')
     plt.show()
         
 
 
 
-image,rotated_image = image_input()
-image_show(image,rotated_image)
+image,rotated_image,image_rotation_angle= image_input()
+image_show(image,rotated_image,image_rotation_angle)
