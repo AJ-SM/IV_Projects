d214daa9a55fe0b547c95b20992bc8663f312b8a
AJ-SM <154649323+AJ-SM@users.noreply.github.com>
2024-02-13 15:04:43 +0530

    Aditional Changes Added
    Changes

--- Visual_Query_Engine_OpenCv/main.py
+++ Visual_Query_Engine_OpenCv/main.py
@@ -18,7 +18,7 @@ file_names = []
 
 
 # Threshold Value For Better  Comparison Of Images 
-threshold_value = 0.80
+threshold_value = 0.45 # float 0.xy x,y = any number 
 
 
 # Grabing Image File If Images Are Greate Than One Then Throwing Error 
@@ -35,15 +35,16 @@ else:
 
 # Walking Through Each Image File , Comparing And Storing The Output Value Into Succed Match
 for root, dirs, files in os.walk(images_folder):
+    print(" ")
  
     for file in files:
-        # Calculating Result By Comparision 
         samples = os.path.join(root, file)
     
         result = Compare_image(target,samples)
 
+        # Calculating Result By Comparision 
         if result >=float(threshold_value):
-            Succed_Match.update({file : result})
+            Succed_Match.update({file : round(result,2)})
 
 
 # Sorting The Dictonary For Better Comparion
@@ -51,7 +52,8 @@ Succed_Match = {k: v for k, v in sorted(Succed_Match.items(), key=lambda item: i
 
 
 #Printing The Dict Of Succed_Match
-print(Succed_Match)
+for images, result in Succed_Match.items():
+    print(f"{images} Matches {(result * 100)} % To Given Image !!!")
 
 
 
