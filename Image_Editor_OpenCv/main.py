d8a7e2e43b0ba7945a6f361d0311f2ce9b634e6e
Anuj <154649323+AJ-SM@users.noreply.github.com>
2024-02-29 08:39:55 +0530

    Update main.py

--- Image_Editor_OpenCv/main.py
+++ Image_Editor_OpenCv/main.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plot
 from InterFace import Interface
 import matplotlib.image as img
 
-
+# Calling The Interface File (Done To Add Further Funcationallity)
 Interface()
 
 
