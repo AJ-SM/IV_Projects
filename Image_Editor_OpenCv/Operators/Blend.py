99ae049e595f5fb9bee442ca8e6138aeeaa9957a
Anuj <154649323+AJ-SM@users.noreply.github.com>
2024-02-29 09:09:00 +0530

    Update Blend.py

--- Image_Editor_OpenCv/Operators/Blend.py
+++ Image_Editor_OpenCv/Operators/Blend.py
@@ -1,3 +1,4 @@
+
 import numpy as np
 import cv2 as cv
 from matplotlib import image as img, pyplot as plt
@@ -7,25 +8,32 @@ from matplotlib import image as img, pyplot as plt
 
 
 
-# Defining Blending Function -- > 
 
+# Image_Blend function take 2 images of same size !! 
 def Img_Blend(img1,img2):
+
+                # Reading Images   
                 img1 = img.imread(img1)
                 img2 = img.imread(img2)
-   
-                alpha = float(input("Enter Weight_1 : "))
-                beta = 1 - alpha
-                #beta = float(input("Enter Weight_2 : "))
-                blend=(alpha*img1 + beta*img2)
-                blend=(np.clip(blend,0,255))
 
+                # Getting Alpha Value <1
+                alpha = float(input("Enter Weight_1 (alpha < 1 ) : "))
+                beta = 1 - alpha
+
+                # Creating a blend image array by muyltipling contasts (alpha , beta) and adding the matrix  
+                blend=(alpha*img1 + beta*img2)
+                blend=(np.clip(blend,0,255)) # Restricting the out ranging value 
+
+
+                # Simpliy Showing Image
                 show_blend(img1,img2,blend)   
 
 def show_blend(img1,img2,blend):
         plt.suptitle("Blending")
         plt.subplot(2,2,1)
         plt.title("Image1" )
-        plt.imshow(img1)
+        plt.imshow(img1) 
+  
 
         plt.subplot(2,2,2)
         plt.title("Image2")
